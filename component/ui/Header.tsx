6b41f8f680d5288d13c658fdc4f64a92d6f3166e
Alexandre DESIR <alexandre.desir@icloud.com>
2022-06-23 13:59:58 -0400

    css navbar

--- component/ui/Header.tsx
+++ component/ui/Header.tsx
@@ -6,12 +6,14 @@ import { useStateContext } from '../context/context'
 const Header = () => {
   const {setShowCart,showCart} = useStateContext()
   return (
-    <nav>
+    <nav className='container'>
+      <div>
       <Link href={'/' }>
-      <a>Fake Store Online </a>
+      <a className='block'>Fake Store Online </a>
       </Link>
+      </div>
       <ul>
-        <Link href={'/'}>
+        <Link href={'/products'}>
         <a>Products </a>
         </Link>
         <Link href={'/'}>
@@ -23,9 +25,38 @@ const Header = () => {
       </ul>
 
       <div>
-        <button onClick={() => setShowCart(true)}> panier</button>
+        <button className='btn' onClick={() => setShowCart(true)}> Cart </button>
       </div>
       {showCart && <Cart/>}
+
+      <style jsx>
+        {
+          `.container{
+            display:flex;
+            background-color:salmon;
+            justify-content:space-between;
+            justify-items:center;
+            color: white;
+          }
+          .container div{
+            display:flex;
+            justify-items:center;
+            justify-content:space-between;
+          }
+          .block{
+            padding:15px 5px;
+          }
+
+          .btn{
+            border:transparent;
+            border-radius:5px;
+            background-color: cornflowerblue;
+
+            width: 100px;
+            color: white;
+          }`
+        }
+      </style>
     </nav>
   )
 }
