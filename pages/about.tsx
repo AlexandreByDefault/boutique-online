4d8c03daf55711f2ed2687cd6118649a7abdbbbd
Alexandre DESIR <alexandre.desir@icloud.com>
2022-06-28 20:28:30 -0400

    about to end 10

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -6,16 +7,33 @@ const about = (props: Props) => {
   return (
     <div className='container'>
       <p>This Page was made with the FakeStopeAi for my portfolio</p>
+      <div className='link'>
+      <Link href={'https://github.com/AlexandreByDefault'}>
+      <a>GITHUB</a>
+      </Link>
+      <Link href={'https://www.linkedin.com/in/alexandre-desir-3200811b2/'}>
+      <a>LinkedIn</a>
+      </Link>
+      <a href='mailto:alexandre.desir@icloud.com'>My mail</a>
+
+      </div>
+
       <style jsx>
         {
           `.container{
             height:90vh;
             text-align:center;
+          }
+          .container p{
+            font-size:clamp(16px,4rem,10rem);
+          }
+          .link{
             display:flex;
             justify-content:center;
-            align-items:center;
-            margin:auto;
-            padding:initial;
+            flex-direction:column;
+            background-color:black;
+            color:white;
+            font-size:clamp(16px,4rem,10rem);
           }`
         }
       </style>
